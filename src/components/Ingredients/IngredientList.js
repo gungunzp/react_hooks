16daa708fd786ed1e643f9a7ca11b4bd7b0f9334
gungunzp <flavazp@gmail.com>
2019-11-20 16:32:36 +0200

    yt1: add scroll-indicator; change app title

--- src/components/Ingredients/IngredientList.js
+++ src/components/Ingredients/IngredientList.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './IngredientList.css';
 
 const IngredientList = props => {
   console.log('RENDERING INGREDIENT LIST');
+
+  const [scrollDepth, setScrollDepth] = useState(0);
+
+  const determineUserScrollDepth = () => {
+    const scrolled =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    setScrollDepth(scrolled);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', determineUserScrollDepth);
+    return () => window.removeEventListener('scroll', determineUserScrollDepth);
+  });
 
   return (
     <section className="ingredient-list">
@@ -16,6 +29,9 @@ const IngredientList = props => {
           </li>
         ))}
       </ul>
+      <div className="scroll-indicator">
+        You've scrolled this far: {scrollDepth}
+      </div>
     </section>
   );
 };
